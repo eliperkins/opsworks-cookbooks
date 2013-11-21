ba1ec4b29ac665db173eb23f2827ed86705b4548
Eli Perkins <eli@onemightyroar.com>
2013-11-21 18:19:16 -0500

    Customizing some more for opsworks

--- sidekiq/recipes/default.rb
+++ sidekiq/recipes/default.rb
@@ -2,54 +2,51 @@
 # Cookbook Name:: sidekiq
 # Recipe:: default
 #
-role = node[:instance_role]
-if role == 'solo' || (role == 'util' && node[:name] =~ /sidekiq/)
 
-  # for now
-  worker_count = 1
+worker_count = 1
 
-  node[:applications].each do |app, data|
-    template "/etc/monit.d/sidekiq_#{app}.monitrc" do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source "monitrc.conf.erb" 
-      variables({ 
-        :num_workers => worker_count,
-        :app_name => app, 
-        :rails_env => node[:environment][:framework_env] 
-      }) 
+node[:deploy].each do |application, deploy|
+  template "/etc/monit.d/sidekiq_#{app}.monitrc" do 
+    owner 'root' 
+    group 'root' 
+    mode 0644 
+    source "monitrc.conf.erb" 
+    variables({ 
+      :num_workers => worker_count,
+      :app_name => app, 
+      :rails_env => deploy[:rails_env] 
+    }) 
+  end
+
+  template "/usr/local/bin/sidekiq" do
+    owner 'root'
+    group 'root' 
+    mode 0755
+    source "sidekiq.erb" 
+  end
+
+  worker_count.times do |count|
+    template "#{deploy[:deploy_to]}/shared/config/sidekiq_#{count}.yml" do
+      group deploy[:group]
+      owner deploy[:user]
+
+      mode 0644
+      source "sidekiq.yml.erb"
+      variables({
+        :require => "#{deploy[:deploy_to]}/current"
+      })
     end
+  end
 
-    template "/usr/local/bin/sidekiq" do
-      owner 'root'
-      group 'root' 
-      mode 0755
-      source "sidekiq.erb" 
-    end
+  execute "ensure-sidekiq-is-setup-with-monit" do 
+    command %Q{ 
+      monit reload 
+    } 
+  end
 
-    worker_count.times do |count|
-      template "/srv/www/#{app}/shared/config/sidekiq_#{count}.yml" do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        source "sidekiq.yml.erb"
-        variables({
-          :require => "/data/#{app}/current"
-        })
-      end
-    end
-
-    execute "ensure-sidekiq-is-setup-with-monit" do 
-      command %Q{ 
-        monit reload 
-      } 
-    end
-
-    execute "restart-sidekiq" do 
-      command %Q{ 
-        echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now 
-      }
-    end
-  end 
+  execute "restart-sidekiq" do 
+    command %Q{ 
+      echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now 
+    }
+  end
 end
